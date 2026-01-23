2fb52455de28a32f52f58ca3b539a125aafe6682
jakobdubeau <jakobdubeau@gmail.com>
2026-01-22 22:05:46 -0500

    Initial Commit

---
+++ Kora/KoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoraApp.swift
+//  Kora
+//
+//  Created by Jakob Dubeau on 2026-01-22.
+//
+
+import SwiftUI
+
+@main
+struct KoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
