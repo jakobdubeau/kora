294d91a4ac69b0a5cd2a3828c7117bff90524162
jakobdubeau <jakobdubeau@gmail.com>
2026-01-24 23:53:28 -0500

    time formatting

--- Kora/Utilities/TimeFormatter.swift
+++ Kora/Utilities/TimeFormatter.swift
@@ -5,3 +5,10 @@
 //  Created by Jakob Dubeau on 2026-01-22.
 //
 
+import Foundation
+
+func formatTime(seconds: Double) -> String {
+    let minutes = Int(seconds) / 60
+    let remainingSeconds = Int(seconds) % 60
+    return String(format: "%02d:%02d", minutes, remainingSeconds)
+}
