97f46265fb82c589c175eed78f57e7a43a5daf4e
jakobdubeau <jakobdubeau@gmail.com>
2026-01-31 02:04:06 -0500

    initial course model

--- Kora/Models/SwiftData/CourseModel.swift
+++ Kora/Models/SwiftData/CourseModel.swift
@@ -6,3 +6,19 @@
 //
 
 // local persisted course/subject, home list of courses
+
+import SwiftData
+
+@Model // persistent data model macro, make it storable in db/on disk
+class Course {
+    var name: String // column in swiftdata store, automatically observed
+    var colour: String?
+    var dailyTotal: Int
+    
+    
+    init(name: String, colour: String? = nil, dailyTotal: Int = 0) {
+        self.name = name
+        self.colour = colour
+        self.dailyTotal = dailyTotal
+    }
+}
