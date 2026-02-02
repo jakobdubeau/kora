a8c6b9267a81e7fbe6f176ab9bde8bdb94d74f18
jakobdubeau <jakobdubeau@gmail.com>
2026-02-01 22:38:18 -0500

    create home view model with demo course

--- Kora/ViewModels/HomeViewModel.swift
+++ Kora/ViewModels/HomeViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 // list of courses, totals, timers
+
+import Foundation
+import Observation
+
+// viewmodel creates engine, viewmodel is the only layer that talks to engine directly, the view goes through the viewmodel
+final class HomeViewModel {
+    
+    let timer = TimerEngine()
+    
+    let demoCourseID = UUID()
+    
+    var demoCourseTime: TimeInterval {
+        timer.courseTime(for: demoCourseID)
+    }
+    
+    var totalTime: TimeInterval { timer.totalTime }
+    var breakTime: TimeInterval { timer.breakTime }
+    
+    var isRunning: Bool {
+        timer.runningCourse == demoCourseID
+    }
+    
+    func toggleCourse() {
+        timer.toggleCourse(demoCourseID)
+    }
+    
+    func newDay() {
+        timer.newDay()
+    }
+    
+}
