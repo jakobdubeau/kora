840dc8d4a4418ac97f36e731c8cec7cf2bbc86c7
jakobdubeau <jakobdubeau@gmail.com>
2026-06-05 19:06:44 -0400

    heatmap utility function

--- Kora/Utilities/HeatmapMapper.swift
+++ Kora/Utilities/HeatmapMapper.swift
@@ -5,3 +5,38 @@
 //  Created by Jakob Dubeau on 2026-01-22.
 //
 
+import Foundation
+
+// snapshot of session data for visual time block
+struct SessionBlock {
+    let duration: TimeInterval
+    let name: String
+    let colour: String?
+}
+
+// get daily hours and sessions
+struct HeatmapMapper {
+    static func map(sessions: [StudySession], courses: [Course]) -> (dailyTotals: [Date: TimeInterval], dailySessions: [Date: [SessionBlock]]) {
+        
+        var dailyTotals: [Date: TimeInterval] = [:]
+        var dailySessions: [Date: [SessionBlock]] = [:]
+        
+        var courseLookup: [UUID: Course] = [:]
+
+        for course in courses {
+            courseLookup[course.id] = course
+        }
+        
+        for session in sessions {
+            let sessionDay = Calendar.current.startOfDay(for: session.start)
+            guard let courseId = session.courseId,
+                  let sessionCourse = courseLookup[courseId] else { continue }
+            
+            dailyTotals[sessionDay, default: 0] += session.duration.rounded(.down)
+            
+            let block = SessionBlock(duration: session.duration.rounded(.down), name: sessionCourse.name, colour: sessionCourse.colour)
+            dailySessions[sessionDay, default: []].append(block)
+        }
+        return (dailyTotals, dailySessions)
+    }
+}
