eff2a41e9535611c4a9849923e5e4e6cd21ebf64
jakobdubeau <jakobdubeau@gmail.com>
2026-07-21 13:33:24 -0400

    bettering ux

--- Kora/Views/Sheets/EditSessionSheet.swift
+++ Kora/Views/Sheets/EditSessionSheet.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct EditSession: View {
     
+    enum Field: Hashable { case startHour, startMinute, endHour, endMinute }
+    @FocusState private var focusedField: Field?
+    
     let lowerBound: Date
     let upperBound: Date
     let initialStart: Date
@@ -17,12 +20,38 @@ struct EditSession: View {
     let onCancel: () -> Void
     let onSave: (Date, Date) -> Void
     
-    @State private var startHour: Int = 0
-    @State private var startMinute: Int = 0
-    @State private var endHour: Int = 0
-    @State private var endMinute: Int = 0
+    @State private var startHour: String = ""
+    @State private var startMinute: String = ""
+    @State private var endHour: String = ""
+    @State private var endMinute: String = ""
     @State private var startPM: Bool = false
     @State private var endPM: Bool = false
+    
+    private var initialStartHour: String {
+        let startTime = Calendar.current.dateComponents([.hour, .minute], from: initialStart)
+        let start = startTime.hour ?? 0 // 24-hour time
+        let hour = start % 12 == 0 ? 12 : start % 12
+        return String(format: "%02d", hour)
+    }
+    
+    private var initialStartMinute: String {
+        let startTime = Calendar.current.dateComponents([.hour, .minute], from: initialStart)
+        let minute = startTime.minute ?? 0
+        return String(format: "%02d", minute)
+    }
+    
+    private var initialEndHour: String {
+        let endTime = Calendar.current.dateComponents([.hour, .minute], from: initialEnd)
+        let end = endTime.hour ?? 0 // 24-hour time
+        let hour = end % 12 == 0 ? 12 : end % 12
+        return String(format: "%02d", hour)
+    }
+    
+    private var initialEndMinute: String {
+        let endTime = Calendar.current.dateComponents([.hour, .minute], from: initialEnd)
+        let minute = endTime.minute ?? 0
+        return String(format: "%02d", minute)
+    }
     
     var body: some View {
         VStack(spacing: 24) {
@@ -54,18 +83,49 @@ struct EditSession: View {
                             .fontDesign(.monospaced)
                     }
                 }
-                
-                TextField("", text: Binding(
-                    get: { String(format: "%02d:%02d", startHour, startMinute) },
-                    set: {
-                        startHour = Int(String($0.prefix(2))) ?? startHour
-                        startMinute = Int(String($0.suffix(2))) ?? startMinute
-                    }
-                ))
-                .font(.system(size: 34, weight: .regular))
-                .fontDesign(.monospaced)
-                .keyboardType(.numberPad)
-                .multilineTextAlignment(.trailing)
+                // start time
+                HStack(spacing: 0) {
+                    TextField("", text: $startHour)
+                        .focused($focusedField, equals: .startHour)
+                        .font(.system(size: 34, weight: .regular))
+                        .foregroundStyle(.white)
+                        .fontDesign(.monospaced)
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.trailing)
+                        .frame(width: 44)
+                        .overlay(alignment: .trailing) {
+                            if startHour.isEmpty {
+                                Text(initialStartHour)
+                                    .font(.system(size: 34, weight: .regular))
+                                    .fontDesign(.monospaced)
+                                    .foregroundStyle(Color(.separator))
+                                    .allowsHitTesting(false)
+                            }
+                        }
+                    
+                    Text(":")
+                        .font(.system(size: 34))
+                        .fontDesign(.monospaced)
+                        .foregroundStyle(startHour.isEmpty || startMinute.isEmpty ? Color(.separator) : .white)
+                    
+                    TextField("", text: $startMinute)
+                        .focused($focusedField, equals: .startMinute)
+                        .font(.system(size: 34, weight: .regular))
+                        .foregroundStyle(.white)
+                        .fontDesign(.monospaced)
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 44)
+                        .overlay(alignment: .leading) {
+                            if startMinute.isEmpty {
+                                Text(initialStartMinute)
+                                    .font(.system(size: 34, weight: .regular))
+                                    .fontDesign(.monospaced)
+                                    .foregroundStyle(Color(.separator))
+                                    .allowsHitTesting(false)
+                            }
+                        }
+                }
                 .frame(maxWidth: .infinity, alignment: .trailing)
             }
 
@@ -94,17 +154,49 @@ struct EditSession: View {
                     }
                 }
 
-                TextField("", text: Binding(
-                    get: { String(format: "%02d:%02d", endHour, endMinute) },
-                    set: {
-                        endHour = Int(String($0.prefix(2))) ?? endHour
-                        endMinute = Int(String($0.suffix(2))) ?? endMinute
-                    }
-                ))
-                .font(.system(size: 34, weight: .regular))
-                .fontDesign(.monospaced)
-                .keyboardType(.numberPad)
-                .multilineTextAlignment(.trailing)
+                // end time
+                HStack(spacing: 0) {
+                    TextField("", text: $endHour)
+                        .focused($focusedField, equals: .endHour)
+                        .font(.system(size: 34, weight: .regular))
+                        .foregroundStyle(.white)
+                        .fontDesign(.monospaced)
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.trailing)
+                        .frame(width: 44)
+                        .overlay(alignment: .trailing) {
+                            if endHour.isEmpty {
+                                Text(initialEndHour)
+                                    .font(.system(size: 34, weight: .regular))
+                                    .fontDesign(.monospaced)
+                                    .foregroundStyle(Color(.separator))
+                                    .allowsHitTesting(false)
+                            }
+                        }
+                    
+                    Text(":")
+                        .font(.system(size: 34))
+                        .fontDesign(.monospaced)
+                        .foregroundStyle(endHour.isEmpty || endMinute.isEmpty ? Color(.separator) : .white)
+                    
+                    TextField("", text: $endMinute)
+                        .focused($focusedField, equals: .endMinute)
+                        .font(.system(size: 34, weight: .regular))
+                        .foregroundStyle(.white)
+                        .fontDesign(.monospaced)
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 44)
+                        .overlay(alignment: .leading) {
+                            if endMinute.isEmpty {
+                                Text(initialEndMinute)
+                                    .font(.system(size: 34, weight: .regular))
+                                    .fontDesign(.monospaced)
+                                    .foregroundStyle(Color(.separator))
+                                    .allowsHitTesting(false)
+                            }
+                        }
+                }
                 .frame(maxWidth: .infinity, alignment: .trailing)
             }
 
@@ -137,18 +229,13 @@ struct EditSession: View {
                 .fill(Color(.secondarySystemBackground))
         )
         .onAppear {
-            // convert swift Date into readable hour/mins
             let startTime = Calendar.current.dateComponents([.hour, .minute], from: initialStart)
-            let start = startTime.hour ?? 0 // 24-hour time
+            let start = startTime.hour ?? 0
             startPM = start >= 12
-            startHour = start % 12 == 0 ? 12 : start % 12
-            startMinute = startTime.minute ?? 0
 
             let endTime = Calendar.current.dateComponents([.hour, .minute], from: initialEnd)
             let end = endTime.hour ?? 0
             endPM = end >= 12
-            endHour = end % 12 == 0 ? 12 : end % 12
-            endMinute = endTime.minute ?? 0
         }
     }
 
